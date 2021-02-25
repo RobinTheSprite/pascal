584ff28c352ca800024217ccf17fafb4242eef82
RobinTheSprite <mark.t.underwood@protonmail.com>
2021-02-24 17:24:58 -0900

    Delete files before recompiling

--- run.bash
+++ run.bash
@@ -1,3 +1,6 @@
+rm pascal
+rm pascal.tab.*
+rm lex.pascal.*
 bison -d pascal.y -o pascal.tab.cpp --defines=pascal.tab.hpp
 flex -o lex.pascal.cpp pascal.l
 g++ -Wall -std=c++11 pascal.tab.cpp lex.pascal.cpp -o pascal -lfl
